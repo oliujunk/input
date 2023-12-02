0c2533b08f859571a83f28c054caf6fe698f2ec5
oliujunk <413041277@qq.com>
2023-12-02 18:36:47 +0800

    Changes

--- rn_2184568_history/rn_2184568_history.go
+++ rn_2184568_history/rn_2184568_history.go
@@ -17,9 +17,7 @@ var (
 	devices []api.Device
 
 	token    string
-	weathers = [...]int{16069548, 16069547, 16069549}
-	waters   = [...]int{16069551, 16069554, 16069557, 16069561, 16069562, 16069569, 16069571}
-	soils    = [...]int{16069586, 16069583, 16069584, 16069585, 16069587, 16069589, 16069592, 16069594, 16069595, 16069597, 16069600, 16069601, 16069602}
+	weathers = [...]int{20201794, 56291669, 56499189, 22018602, 22018603, 22018604, 22018605, 22018606, 22018607, 22018608}
 )
 
 func updateToken() {
@@ -37,35 +35,30 @@ func Start() {
 	}
 	log.Println("2184568历史数据补充")
 	updateToken()
-	updateDevices()
 
-	for _, device := range devices {
-		log.Println(device.DeviceID)
-		if device.DeviceID != 16069573 {
-			collection := client.Database("xph").Collection(strconv.Itoa(device.DeviceID))
-			opts := options.FindOne().SetSort(bson.D{{"dataTime", -1}})
-			var result api.CurrentData
-			err := collection.FindOne(context.TODO(), bson.D{{"facId", device.DeviceID}}, opts).Decode(&result)
-			if err != nil {
-				continue
-			}
-			//fmt.Printf("found document %v", result)
-			start, err := time.Parse("2006-01-02 15:04:05", "2023-04-01 00:00:00")
-			end, err := time.Parse("2006-01-02 15:04:05", "2021-05-01 00:00:00")
-			if err != nil {
-				continue
-			}
-			for start.After(end) {
-				start = start.Add(-60 * time.Minute)
-				result.Datatime = start.Format("2006-01-02 15:04:05")
-				one, err := collection.InsertOne(context.TODO(), result)
-				if err != nil {
-					continue
-				}
-				fmt.Println(result, one)
-			}
-
-			time.Sleep(1 * time.Second)
+	for _, device := range weathers {
+		collection := client.Database("xph").Collection(strconv.Itoa(device))
+		opts := options.FindOne().SetSort(bson.D{{"dataTime", -1}})
+		var result api.CurrentData
+		err := collection.FindOne(context.TODO(), bson.D{{"facId", device}}, opts).Decode(&result)
+		if err != nil {
+			continue
 		}
+		//fmt.Printf("found document %v", result)
+		start, err := time.Parse("2006-01-02 15:04:05", "2023-04-01 00:00:00")
+		end, err := time.Parse("2006-01-02 15:04:05", "2021-05-01 00:00:00")
+		if err != nil {
+			continue
+		}
+		for start.After(end) {
+			start = start.Add(-60 * time.Minute)
+			result.Datatime = start.Format("2006-01-02 15:04:05")
+			one, err := collection.InsertOne(context.TODO(), result)
+			if err != nil {
+				continue
+			}
+			fmt.Println(result, one)
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
